094d985495bf7034ae1ec65a29f7a8b038ac3935
Josh Theis <cdg.josh@googlemail.com>
2013-11-20 00:53:27 +0100

    corrected class

--- src/cdg/nut/util/MouseButtons.java
+++ src/cdg/nut/util/MouseButtons.java
@@ -3,6 +3,6 @@ package cdg.nut.util;
 public final class MouseButtons {
 	
 	public static final int LEFT = 0;
-	public static final int MIDDLE = 1;
-	public static final int RIGHT = 2;
+	public static final int RIGHT = 1;
+	public static final int MIDDLE = 2;
 }
