07a08f9b12aae16c8b484ef8ecf138fac91d9786
Josh Theis <cdg.josh@googlemail.com>
2013-11-20 00:52:31 +0100

    wip: scrollbars

--- src/cdg/nut/gui/HScrollBar.java
+++ src/cdg/nut/gui/HScrollBar.java
@@ -1,5 +1,23 @@
 package cdg.nut.gui;
 
-public class HScrollBar {
+public class HScrollBar extends ScrollBar {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setupMainGL() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setupSelectionGL() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
